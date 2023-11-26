204baaf61b29bf1d886064bed580c3801846fb9e
agschmid <agschmid@andrew.cmu.edu>
2023-11-25 21:48:55 -0800

    more style changes

--- src/CoolCanvas.js
+++ src/CoolCanvas.js
@@ -5,12 +5,12 @@ import {useRef} from "react";
 
 
 export default function CoolCanvas(){
-    const childRef = useRef(null);
+    const childRef = [useRef(0), useRef(1), useRef(2)];
 
     return (
         <div id='cool-canvas'>
             <GradientElement></GradientElement>
-            <Canvas camera={{ fov: 40, position: [0,0,10]}} id='text' width='100%' onPointerOver={() => (childRef.current.extruded = true)} onPointerOut={() => (childRef.current.extruded = false)}>
+            <Canvas camera={{ fov: 40, position: [0,0,10]}} id='text' width='100%' onClick = {()=>childRef[0].current.currentText = (childRef[0].current.currentText+1)%3} onPointerOver={() => (childRef[0].current.extruded = true)} onPointerOut={() => (childRef[0].current.extruded = false)}>
             <Real forwardedRef={childRef}></Real>
             </Canvas>
         </div>
--- src/Real.js
+++ src/Real.js
@@ -1,32 +1,68 @@
-import React from "react";
+import React , {useRef} from "react";
 import {useFrame } from "@react-three/fiber";
 import { Text3D, Center } from "@react-three/drei";
+import { BlendFunction } from 'postprocessing'
+import { EffectComposer, Noise } from "@react-three/postprocessing";
+
 
 export default function Real(props) {
   const ref = props.forwardedRef
+  const meshRef = [useRef(0), useRef(1), useRef(2)]
 
   useFrame((_, delta) => {
-    if (ref.current.extruded && ref.current.scale.z < 100){
-      ref.current.scale.z += delta*400 * -0.3*Math.log(ref.current.scale.z/100)
+    for (let i=0; i<meshRef.length; i++){
+      if (i === ref[0].current.currentText){
+        ref[i].current.extruded = true;
+      } else {
+        ref[i].current.extruded = false;
+      }
+
+      if (ref[i].current.extruded && ref[i].current.scale.z < 80){
+        ref[i].current.scale.z += delta*400 * -0.1*Math.log(ref[i].current.scale.z/80)
+      }
+      if (!ref[i].current.extruded && ref[i].current.scale.z > 1){
+        ref[i].current.scale.z -= delta*50
+      }
+      ref[i].current.scale.z = Math.max(Math.min(ref[i].current.scale.z, 80), 1)
+      meshRef[i].current.color.r = 1-(ref[i].current.scale.z/150)
+      meshRef[i].current.color.g = 1-(ref[i].current.scale.z/150)
+      meshRef[i].current.color.b = 1-(ref[i].current.scale.z/300)
     }
-    if (!ref.current.extruded && ref.current.scale.z > 1){
-      ref.current.scale.z -= delta*300
-    }
-    ref.current.scale.z = Math.max(Math.min(ref.current.scale.z, 100), 1)
-    ref.current.rotation.x = -ref.current.scale.z*0.002
-    ref.current.rotation.y = -ref.current.scale.z*0.0005
 
   })
 
   return(
     <>
       <Center middle>
-        <Text3D ref={ref} font= './roboto.json' size={5} height={0.01} extruded={false} >
+        <Text3D ref={ref[0]} font= './roboto.json' size={5} height={0.01} extruded={false} currentText={0}>
         REAL
-        <meshMatcapMaterial color="white"/>
-      </Text3D>
+        <meshMatcapMaterial ref={meshRef[0]} color = {'white'}/>
+        </Text3D>
       </Center>
+      <Center middle>
+        <Text3D ref={ref[1]} font= './roboto.json' size={5} height={0.01}>
+        REFINED
+        <meshMatcapMaterial ref={meshRef[1]} color = {'white'}/>
+        </Text3D>
+      </Center>
+      <Center middle>
+        <Text3D ref={ref[2]} font= './roboto.json' size={5} height={0.01}>
+        RESONATE
+        <meshMatcapMaterial ref={meshRef[2]} color = {'white'}/>
+        </Text3D>
+      </Center>
+
+      <mesh>
+        <planeGeometry args={[100, 10]}/>
+        <meshMatcapMaterial 
+        color={'white'}
+        />
+      </mesh>
+
       {/* <OrbitControls /> */}
+      <EffectComposer>
+          <Noise premultiply blendFunction={BlendFunction.ADD} />
+      </EffectComposer>
     </>
   )
 }
